85ee148dc11f2995a0f2a8c712f6704fc1758237
YektaAnilAksoy <yektaanilaksoy@hotmail.com.tr>
2020-12-26 22:18:21 +0300

    initial commit

---
+++ src/main/java/com/yektaanil/linksepeti/LinksepetiApplication.java
@@ -0,0 +1,13 @@
+package com.yektaanil.linksepeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksepetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksepetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yektaanil/linksepeti/LinksepetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yektaanil.linksepeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksepetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
